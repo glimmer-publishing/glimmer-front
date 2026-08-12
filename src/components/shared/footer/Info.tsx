a82b5c65b23129824326e7219fc61d7e48f2b77c
Oleksandr Humeniuk <ole.humeniuk@foodtech.team>
2026-08-12 14:14:15 +0300

    Change phone number

--- src/components/shared/footer/Info.tsx
+++ src/components/shared/footer/Info.tsx
@@ -11,7 +11,7 @@ export default function Info() {
           rel="noopener noreferrer nofollow"
           className="block mb-10 text-main text-[16px] lg:text-[18px] font-semibold active:brightness-125 xl:hover:brightness-125 focus-visible:brightness-125 transition duration-300 ease-in-out"
         >
-          0 77 837 84 53
+          073 919 25 23
         </a>
 
         <p className="text-white text-[16px] lg:text-[18px] font-semibold">
